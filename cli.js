81d901cc51c804dbcd8b26f25734f16c0ead6b29
Marcus Zanona <zanona@me.com>
2015-02-28 17:36:09 +0000

    Improve CLI instructions

--- cli.js
+++ cli.js
@@ -1,3 +1,31 @@
 #!/usr/bin/env node
-/*global require, process*/
-require('./')(process.argv[2], process.argv[3]);
+/*jslint node:true*/
+(function () {
+    'use strict';
+    var log = require('./utils/log'),
+        pkg = require('./package'),
+        run = require('./'),
+        args = process.argv.splice(2);
+
+    function version() {
+        log.info(
+            '%s - %s \nVersion: %s',
+            pkg.name.replace(/^\w/, function (m) { return m.toUpperCase(); }),
+            pkg.description,
+            pkg.version
+        );
+    }
+
+    function help() {
+        version();
+        log.info(
+            'Usage:',
+            'pakku <path-to-index-page.html> <build-directory>'
+        );
+    }
+
+    if (args[0] === '-v') { return version(); }
+    if (args[0] === '-h' || args.length < 2) { return help(); }
+
+    run.apply(null, args);
+}());
