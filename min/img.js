94b0d45df4a02a79acf9f5c4ebbc2a27542d438a
Marcus Zanona <zanona@me.com>
2015-11-13 17:22:22 +0000

    Skip compression on imagemin error

--- min/img.js
+++ min/img.js
@@ -1,3 +1,4 @@
+/*jslint node:true*/
 module.exports = function (files) {
     'use strict';
     var Q = require('q'),
@@ -10,7 +11,12 @@ module.exports = function (files) {
             originalSize = file.contents.length;
 
         imagemin.src(file.contents).run(function (e, files) {
-            if (e) { return df.reject(e); }
+            if (e) {
+                log.error(e);
+                log.error('[%s] ERROR on compression, skipping', file.name);
+                return df.resolve(df);
+                //return df.reject(e);
+            }
 
             var compressedSize = files[0].contents.length,
                 compression = 100
