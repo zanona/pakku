864fea544c721dd4cf76852c6431b994c0cd8198
Marcus Zanona <zanona@me.com>
2016-04-30 10:57:59 +0100

    Add support for root URLs (/) (/file.html)

--- utils/resolve.js
+++ utils/resolve.js
@@ -1,23 +1,35 @@
-module.exports = function (href, parentSrc) {
-    'use strict';
+var url  = require('url'),
+    path = require('path'),
+    ft   = require('./filetype');
 
+module.exports = function (href, parentSrc) {
     href      = (href || './').toString();
     parentSrc = (parentSrc || './').toString();
 
-    var url = require('url'),
-        //path = require('path'),
-        ft = require('./filetype'),
-        uri = url.resolveObject(parentSrc, href),
-        file = {
-            name: uri.pathname,
-            external: !!(uri.slashes || uri.protocol),
-            type: ft(href),
-            //ext: path.parse(uri.pathname).ext,
-            href: href, //uri.href is not the original (path resolved),
-            query: (uri.search || '') + (uri.hash || '')
-            //path: path.resolve(uri.pathname)
-        };
+    var dir       = process.cwd(),
+        parentDir = path.dirname(parentSrc),
+        uri,
+        file;
 
+    /* IF PATH IS / ASSUME HTML AND FALLBACK TO INDEX FILE */
+    if (href === '/') { href = '/index.html'; }
+    /* IF PATH STARTS WITH / PREPEND PROCESS.CWD */
+    if (href.match(/^\//)) {
+        href = dir + href;
+        href = path.relative(parentDir, href);
+    }
+
+    uri = url.resolveObject(parentSrc, href),
+    file = {
+        name: uri.pathname,
+        external: !!(uri.slashes || uri.protocol),
+        type: ft(href),
+        //ext: path.parse(uri.pathname).ext,
+        href: href, //uri.href is not the original (path resolved),
+        query: (uri.search || '') + (uri.hash || '')
+        //path: path.resolve(uri.pathname)
+    };
+    return file;
     /* {
         name: 'index.html',
         external: false,
@@ -27,6 +39,4 @@ module.exports = function (href, parentSrc) {
         path: '/Users/name/Desktop/lib/test/index.html'
     }*/
     //external: /^[\w\-]+:|^\/{2}|^[^\.\w]/.test(href)
-
-    return file;
 };
