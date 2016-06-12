90e9908175ca5e98dad5798d561c7a4fba5aad77
Marcus Zanona <zanona@me.com>
2016-06-12 22:15:53 +0100

    Add support for .ico files

--- utils/filetype.js
+++ utils/filetype.js
@@ -1,7 +1,7 @@
 module.exports = function (ext) {
     'use strict';
     if (ext.match(/\.(html|xml)$/)) { return 'html'; }
-    if (ext.match(/\.(png|gif|jpg|svg)$/)) { return 'img'; }
+    if (ext.match(/\.(png|gif|jpg|svg|ico)$/)) { return 'img'; }
     if (ext.match(/\.(css|less|scss|sass)$/)) { return 'css'; }
     if (ext.match(/\.(js|json|ld\+json)$/)) { return 'js'; }
     if (ext.match(/\.(txt|md|mdown|markdown|rtf)$/)) { return 'txt'; }
