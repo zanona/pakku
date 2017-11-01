cbbdc54d6fdafeffd582ce748741e40086869b69
Marcus Zanona <marcus@zanona.co>
2017-11-01 16:43:04 -0200

    fix(parser/js): prevent module paths from being rewritten
    This will prevent module importing such as: `require('./index.js')`
    or `import {foo} from './bar.js'` from being rewritten, fixing issues
    where pakku would return errors locating imported modules.

--- parser/js.js
+++ parser/js.js
@@ -1,4 +1,18 @@
-exports.setContent = function (content) { return content; };
+exports.setContent = function (content, file) {
+  /*
+   * replace require or import statements module names ending with
+   * .js or .json so these won't get renamed by the general parser
+   */
+  const requireMatch = /require\((['"])(.*?)\.(?:js|json)\1\)/gm,
+        importMatch  = /import((?:.*?from)? +)(["'])(.+?)\.(?:js|json)\2/gm;
+
+  if (requireMatch.test(content) || importMatch.test(content)) {
+      file.hasImports = true;
+  }
+
+  return content.replace(requireMatch, 'require($1$2$1)')
+                .replace(importMatch,  'import$1$2$3$2');
+};
 exports.setResource = function (file, parent) {
     var resolve = require('../utils/resolve');
     if (!parent.name.match(/-html-/)) { file = resolve(file.href, './'); }
