cbcc2a552a60899e79b944fefe303852ac80d597
Marcus Zanona <marcus@zanona.co>
2017-11-09 23:23:17 -0200

    Update image compression logic after imagemin upgrade

--- min/img.js
+++ min/img.js
@@ -1,43 +1,33 @@
 /*jslint node:true*/
 module.exports = function (files) {
-    'use strict';
-    var Q = require('q'),
-        Imagemin = require('imagemin'),
+  'use strict';
+  const imagemin         = require('imagemin'),
+        imageminJpegtran = require('imagemin-jpegtran'),
+        imageminPngquant = require('imagemin-pngquant'),
+        imageminSvgo     = require('imagemin-svgo'),
+        batchPromises    = require('batch-promises'),
+        plugins = [
+          imageminJpegtran(),
+          imageminPngquant({quality: '90'}),
+          imageminSvgo()
+        ],
         log = require('../utils').log;
 
-    function run(file) {
-        var df = Q.defer(),
-            imagemin = new Imagemin(),
-            originalSize = file.contents.length;
+  function run(file) {
+    return imagemin.buffer(file.contents, {plugins}).then((cFile) => {
+      const originalSize = file.contents.length,
+            compressedSize = cFile.length,
+            compression = 100 - Math.ceil((compressedSize * 100) / originalSize);
+      file.contents = cFile;
+      if (compression) log.info('[%s] compressed in %s%', file.name, compression);
+      return file;
+    });
+  }
 
-        imagemin.src(file.contents).run(function (e, files) {
-            if (e) {
-                log.error(e);
-                log.error('[%s] ERROR on compression, skipping', file.name);
-                return df.resolve(df);
-                //return df.reject(e);
-            }
+  function main() {
+    if (!files.map) files = [files];
+    return batchPromises(10, files, run);
+  }
 
-            var compressedSize = files[0].contents.length,
-                compression = 100
-                    - Math.ceil((compressedSize * 100) / originalSize);
-
-            file.contents = files[0].contents;
-            if (compression) {
-                log.info('[%s] compressed in %s%', file.name, compression);
-            }
-            df.resolve(df);
-        });
-
-        return df.promise;
-    }
-
-    function main() {
-        var d = Q.defer();
-        if (!files.map) { files = [files]; }
-        Q.all(files.map(run)).then(d.resolve, d.reject);
-        return d.promise;
-    }
-
-    return main();
+  return main();
 };
