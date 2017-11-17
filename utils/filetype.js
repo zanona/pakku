c5711adb70acc9d57504a2b9dd9c823da6124190
Marcus Zanona <marcus@zanona.co>
2017-11-17 14:57:28 -0200

    refactor(utils): reduce return statements in filetype.js
    resolve #9

--- utils/filetype.js
+++ utils/filetype.js
@@ -1,8 +1,9 @@
 module.exports = (src) => {
-  if (src.match(/\.(html|xml|md|mdown|markdown)$/)) return 'html';
-  if (src.match(/\.(png|gif|jpg|svg|ico)$/)) return 'img';
-  if (src.match(/\.(css|less|scss|sass)$/)) return 'css';
-  if (src.match(/\.(js|json|ld\+json)$/)) return 'js';
-  if (src.match(/\.(txt|rtf)$/)) return 'txt';
-  return 'other';
+  let type = 'other';
+  if (src.match(/\.(html|xml|md|mdown|markdown)$/)) type = 'html';
+  if (src.match(/\.(png|gif|jpg|svg|ico)$/))        type = 'img';
+  if (src.match(/\.(css|less|scss|sass)$/))         type = 'css';
+  if (src.match(/\.(js|json|ld\+json)$/))           type = 'js';
+  if (src.match(/\.(txt|rtf)$/))                    type = 'txt';
+  return type;
 };
