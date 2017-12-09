8d5306ad33034bfb8c4ef6707d7c77e57a182fe3
Marcus Zanona <marcus@zanona.co>
2017-12-09 11:50:36 -0200

    fix(build): build dir created in relation target file and not pwd

--- test/functional/default.js
+++ test/functional/default.js
@@ -1,14 +1,17 @@
-const {test} = require('ava'),
-      fs     = require('fs'),
+const fs     = require('fs'),
+      path   = require('path'),
+      {test} = require('ava'),
       mock   = require('mock-fs'),
       pakku  = require('../../lib'),
-      files  = require('../lib/files'),
-      TARGET_FILE = 'index.html',
-      BUILD_DIR = 'build';
+      db     = require('../lib/files'),
+      CWD         = process.cwd(),
+      BASE_DIR    = 'src/',
+      TARGET_FILE = path.join(BASE_DIR, 'index.html'),
+      BUILD_DIR   = 'build';
 
 let cache;
 
-test.before('setting up fs', mock.bind(this, files));
+test.before('setting up fs', mock.bind(this, db.generateFiles(BASE_DIR)));
 test.cb.before('running pakku', (t) => {
   pakku(TARGET_FILE, BUILD_DIR, {sourcemaps: true})
     .on('after_build', (c) => (cache = c, t.end()))
@@ -16,7 +19,9 @@ test.cb.before('running pakku', (t) => {
 });
 test.beforeEach((t) => t.context.cache = cache);
 
-test.cb('check for target build directory', (t) => fs.stat(BUILD_DIR, t.end));
+test.cb('check for target build directory', (t) => {
+  fs.stat(`${CWD}/${BUILD_DIR}`, t.end);
+});
 
 test('check file structure', (t) => {
   const cachedFiles = Object.values(t.context.cache);
@@ -24,11 +29,6 @@ test('check file structure', (t) => {
   cachedFiles.forEach(({vname}) => {
     t.notThrows(fs.statSync.bind(this, `${BUILD_DIR}/${vname}`));
   });
-});
-
-test('check for built files', (t) => {
-  const file = t.context.cache.find((f) => f.name === 'index.html');
-  return t.true(file.done);
 });
 
 test.after('cleaning up fs', mock.restore);
--- test/lib/files.js
+++ test/lib/files.js
@@ -1,17 +1,29 @@
-const fileCacher = require('./file-cacher.js'),
+const path = require('path'),
+      fileCacher = require('./file-cacher.js'),
       nodeModules  = fileCacher.duplicateFSInMemory('node_modules');
-module.exports = {
-  node_modules: nodeModules,
-  'index.html': `
-    <link rel=stylesheet href=lib/meta/index.less>
-    <script src=lib/meta/index.js></script>
-  `,
-  'lib/meta/index.js': `
-    const msg = 'hello world';
-    console.log(\`\${msg}\`);
-  `,
-  'lib/meta/index.less': `
-    @color: red;
-    body{ background: @color; }
-  `
+function rebasePathForFiles(files, baseDir) {
+  return Object.keys(files).reduce((p, c) => {
+    const f = files[c];
+    if (baseDir && c !== 'node_modules') c = path.join(baseDir, c);
+    p[c] = f;
+    return p;
+  }, {});
+}
+exports.generateFiles = (baseDir) => {
+  const files = {
+    node_modules: nodeModules,
+    'index.html': `
+      <link rel=stylesheet href=lib/meta/index.less>
+      <script src=lib/meta/index.js></script>
+    `,
+    'lib/meta/index.js': `
+      const msg = 'hello world';
+      console.log(\`\${msg}\`);
+    `,
+    'lib/meta/index.less': `
+      @color: red;
+      body{ background: @color; }
+    `
+  };
+  return rebasePathForFiles(files, baseDir);
 };
